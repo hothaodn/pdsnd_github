341f7ddca18e08ecd73a73b8aacedac55faed284
hothaodn <hothaodn@gmail.com>
2024-10-26 18:56:06 +0700

    refactor: Enhance display_raw_data func of bikeshare_git.py

--- bikeshare_git.py
+++ bikeshare_git.py
@@ -240,7 +240,7 @@ def main():
         user_stats(df)
         display_raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Please enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
